0868b8cd7b3b676e5a235e800d71dbc94f353b88
AkashJegar <akash.jegar@cognine.com>
2024-03-05 19:11:29 +0530

    test2

---
+++ cypress/e2e/test.cy.js
@@ -0,0 +1,9 @@
+describe('test', () => {
+    //dfghjk
+      it('Login', () => {
+        cy.visit('https://opensource-demo.orangehrmlive.com/')
+        cy.get('input[name="username"]').type('Admin');
+        cy.get('input[type="password"]').type('admin123');
+        cy.get('button[type="submit"]').click();
+      })
+    })
